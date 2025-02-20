5b795f05c7f172878f820f549ea781f6262778ca
Tom Stian Ingebretson <toms.ingebretson@gmail.com>
2025-02-20 10:19:47 +0100

    copilot Init

---
+++ dot_config/nvim/lua/custom/plugins/copilot.lua
@@ -0,0 +1,14 @@
+function copilot_init()
+	vim.api.nvim_set_keymap("i", "<C-y>", "copilot#Accept('<CR>')", {
+		silent = true,
+		expr = true,
+		noremap = true,
+	})
+	vim.g.copilot_no_tab_map = true
+	vim.g.copilot_assume_mapped = true
+end
+
+return {
+	"github/copilot.vim",
+	init = copilot_init,
+}
